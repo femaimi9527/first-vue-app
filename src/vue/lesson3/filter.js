a3d5152e957b8fc31db34de001c1bec85d7be980
xxm <15007591989@139.com>
2019-10-22 18:52:36 +0800

    Vue本课目标——设计货币转换器，过滤器

---
+++ src/vue/lesson3/filter.js
@@ -0,0 +1,13 @@
+
+Vue.filter("fix3", (val) => {
+    if (!val || isNaN(val)) return 0
+    return val.toFixed(2)
+})
+
+Vue.filter("fmtGender", (val) => {
+    switch (val) {
+        case 0: return "男"
+        case 1: return "女"
+        default: return "未知"
+    }
+})
